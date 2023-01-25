aaf0666507b8f4ea04ce7e6f2241f2e9aa025e19
Jeru2023 <123569003+Jeru2023@users.noreply.github.com>
2023-01-25 23:07:25 +0800

    DB access module

---
+++ mydb.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import configparser as cp
+
+#####################################
+# Database access module for stock code table and stock daily K line table
+# usage:
+# import mydb
+# mydb.function_name(params)
+#####################################
+
+config = cp.ConfigParser()
+config.read('sphinx.config', encoding='utf-8-sig')
+
+# database connection
+def get_connection():
+    user = config.get('DB', 'user')
+    password = config.get('DB', 'password')
+    host = config.get('DB', 'host')    
+    port = config.get('DB', 'port')
+    database = config.get('DB', 'database')
+    return create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(user, password, host, port, database))
+	
+# query full stock list from DB
+def query_stock_list():
+    engine = get_connection()
+    sql = "select * from stock_code;"
+    df = pd.read_sql_query(sql, engine)
+    return df
+	
+# query stock list by industry
+# industry: 申万一级行业名称
+def query_stock_list_by_industry(industry):
+    engine = get_connection()
+    sql = "select * from stock_code where industry='{}';".format(industry)
+    df = pd.read_sql_query(sql, engine)
+    return df
+
+# query stock daily K line data by code
+def query_stock_kline_by_code(code):
+    engine = get_connection()
+    sql = "select * from stock_kline_daily where code='{}';".format(code)
+    df = pd.read_sql_query(sql, engine)
+    return df
+
+# query stock daily K line data by industry
+def query_stock_kline_by_industry(industry):
+    engine = get_connection()
+    sql = "select skd.date, skd.code, sc.code_name, skd.open, skd.close, skd.volume, skd.amount, skd.pctChg, skd.pbMRQ, skd.peTTM from stock_kline_daily skd, stock_code sc where skd.code=sc.code and sc.industry='{}';".format(industry)
+    df = pd.read_sql_query(sql, engine)
+    return df
