a7c9feb9a6b12b3eef7bbd4b502facfa8adb5e22
Jeru2023 <123569003+Jeru2023@users.noreply.github.com>
2023-01-27 10:18:02 +0800

    bug fix

--- strategy_evaluation.py
+++ strategy_evaluation.py
@@ -1,29 +1,17 @@
-import baostock as bs
-import pandas as pd
-import mydb
-import math
-import time
-
 # 捕获单只股票满足策略模型的记录
-# volume_mean_window_len 为对比滚动移动平均线的时间窗口长度，如10则对比过去10日的平均成交量
-# volume_multiple 为当日成交量对比过去mean_window_len范围内平均成交量的倍数
-# exit_window_len 为卖出的时间窗口长度，如-15则在15日后卖出
-def get_capture_record(stock_df, volume_mean_window_len, volume_multiple, exit_window_len, pb_cap=100, pe_cap=100):
+def get_capture_record(stock_df, entry_rule_dict, exit_rule_dict):
+    volume_mean_window_len = rm.entry_rule_dict.get('volume_mean_window_len')
+    exit_window_len = rm.exit_rule_dict.get('exit_window_len')
+    
     stock_df['volume_rolling_mean'] = stock_df['volume'].rolling(volume_mean_window_len).mean()
     stock_df['profit'] = stock_df['close'].shift(exit_window_len)-stock_df['close']
     
     row_list = []
     for index, row in stock_df.iterrows():
-        if (index<volume_mean_window_len):
+        if (index < volume_mean_window_len):
             continue
-        
-        # 放量收阳线
-        if (pb_cap<100):
-            indicator_condition = row['pbMRQ']<pb_cap
-        elif (pe_cap<100):
-            indicator_condition = row['peTTM']<pe_cap
 
-        if (row['volume']>row['volume_rolling_mean']*volume_multiple) and (row['pctChg']>0) and indicator_condition:
+        if (rm.evaluate_entry(row, entry_rule_dict, exit_rule_dict)):
             row_list.append(row)
     return row_list
 
@@ -45,7 +33,7 @@ def evaluate_stock_profit(row_list):
     return stock_profit_ratio, stock_size
 
 # 按行业评估策略模型整体收益
-def evaluate_industry_profit(stock_list_df, volume_mean_window_len, volume_multiple, exit_window_len, pb_cap):
+def evaluate_industry_profit(stock_list_df, entry_rule_dict, exit_rule_dict):
     code_list = stock_list_df['code'].unique()
     
     total_profit_ratio = 0.0
@@ -55,7 +43,7 @@ def evaluate_industry_profit(stock_list_df, volume_mean_window_len, volume_multi
         stock_df = stock_list_df[stock_list_df['code']==code]
         print(stock_df['code_name'].unique(), code)
     
-        row_list = get_capture_record(stock_df, volume_mean_window_len, volume_multiple, exit_window_len, pb_cap)
+        row_list = get_capture_record(stock_df, entry_rule_dict, exit_rule_dict)
         stock_profit_ratio, stock_size = evaluate_stock_profit(row_list)
         
         if (math.isnan(stock_profit_ratio) or stock_size<1):
